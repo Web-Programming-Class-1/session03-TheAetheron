1a5bf87a9eb6885f429787384aeceb4169aeedc9
Kenneth Andrew Lukita <kennethandrew1602@gmail.com>
2025-09-26 11:00:08 +0700

    add project files

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -3,9 +3,59 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Halaman Percobaan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
 <body>
-    
+<a class="btn btn-primary" href="#" role="button">Link</a>
+<button class="btn btn-primary" type="submit">Button</button>
+<input class="btn btn-primary" type="button" value="Input">
+<input class="btn btn-primary" type="submit" value="Submit">
+<input class="btn btn-primary" type="reset" value="Reset">
+
+<a href="https://getbootstrap.com/">Bootstrap</a>
+<a href="https://bulma.io/">Bulma</a>
+
+<form class="box">
+  <div class="field">
+    <label class="label">Email</label>
+    <div class="control">
+      <input class="input" type="email" placeholder="e.g. alex@example.com" />
+    </div>
+  </div>
+
+  <div class="field">
+    <label class="label">Password</label>
+    <div class="control">
+      <input class="input" type="password" placeholder="********" />
+    </div>
+  </div>
+
+  <button class="button is-primary">Sign in</button>
+</form>
 </body>
+<progress class="progress is-small is-primary" max="100">15%</progress>
+<progress class="progress is-danger" max="100">30%</progress>
+<progress class="progress is-medium is-dark" max="100">45%</progress>
+<progress class="progress is-large is-info" max="100">60%</progress>
+
+<div class="row">
+      <div class="col s1">1</div>
+      <div class="col s1">2</div>
+      <div class="col s1">3</div>
+      <div class="col s1">4</div>
+      <div class="col s1">5</div>
+      <div class="col s1">6</div>
+      <div class="col s1">7</div>
+      <div class="col s1">8</div>
+      <div class="col s1">9</div>
+      <div class="col s1">10</div>
+      <div class="col s1">11</div>
+      <div class="col s1">12</div>
+    </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js" defer></script>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,28 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('bulma');
 });
+
+Route::get('/materialize', function () {
+    return view('materialize');
+});
+
+Route::post('/form-submit', function (Request $request) {
+    $name = $request->input('name');
+    $email = $request->input('email');
+    $password = $request->input('password');
+
+    dd([
+        'name' => $name,
+        'email' => $email,
+        'password' => $password
+    ]);
+
+    return back()->with('success', 'Form berhasil dikirim!');
+})->name('form-materialize.submit');
+Route::get('/bulma', function () {
+    return view('bulma');
+});
+
+Route::post('/submit-form', [FormController::class, 'store'])->name('form_bulma.submit');
